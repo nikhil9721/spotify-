4095594cc11f7d1e02b4ffad129dd0335ca476f4
Nikhil kumar rai <nikhilkumarrai7@gmail.com>
2026-09-21 19:46:19 +0530

    create spotify.js

---
+++ spotify.js
@@ -0,0 +1,12 @@
+let pri=["orange","pink","green"];
+let sec=["yellow","brown","violet"];
+let tri=pri.concat(sec);
+console.log(tri);
+console.log(sec);
+console.log(pri);
+let fou=pri.reverse(sec);
+console.log(fou);
+let fiv=pri.splice(0,6,"pink","man");
+console.log(fiv);
+let cars=["A","b","c","n","l"]
+let six=cars.sort();
